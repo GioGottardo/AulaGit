4a7d236c0ca7b3a4aac659dc43e7effde55a5ef4
G <G@DESKTOP-0FP9FRF>
2017-03-30 22:05:37 -0300

    Ultimo comit

--- ProjetoGit/src/br/univel/classe/Principal.java
+++ ProjetoGit/src/br/univel/classe/Principal.java
@@ -2,7 +2,7 @@ package br.univel.classe;
 
 public class Principal {
 	public static void main(String[] args) {
-		String hello = "Hello";
+		String hello = "Hellox";
 		String world = "World";
 		String msg = hello + world + "!";
 		System.out.println(msg);
