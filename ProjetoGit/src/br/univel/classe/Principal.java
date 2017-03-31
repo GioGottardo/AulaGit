aa9b758d75281112027df5a7adf9de98d65b2cc5
G <G@DESKTOP-0FP9FRF>
2017-03-30 21:54:11 -0300

    Terceira Versão.

--- ProjetoGit/src/br/univel/classe/Principal.java
+++ ProjetoGit/src/br/univel/classe/Principal.java
@@ -2,7 +2,7 @@ package br.univel.classe;
 
 public class Principal {
 	public static void main(String[] args) {
-		String msg = "Olá mundo!";
+		String msg = "Hello World";
 		System.out.println(msg);
 	}
 
