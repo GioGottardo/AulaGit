33ebb53fb4f4becd74586990d6933f1347011f1a
G <G@DESKTOP-0FP9FRF>
2017-03-30 21:56:25 -0300

    Alteração após criação da tag.

--- ProjetoGit/src/br/univel/classe/Principal.java
+++ ProjetoGit/src/br/univel/classe/Principal.java
@@ -2,7 +2,8 @@ package br.univel.classe;
 
 public class Principal {
 	public static void main(String[] args) {
-		String msg = "Hello World";
+		String world = "World";
+		String msg = "Hello World" + world + "!";
 		System.out.println(msg);
 	}
 
